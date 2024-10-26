432b795ae39fab36db52ca851238e4536de1e379
salahudheenp <salahuznr@gmail.com>
2024-10-26 19:32:04 +0530

    grid added

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,20 @@
+import Grid from "@/components/Grid";
 import Hero from "@/components/Hero";
 // import { FloatingNav } from "@/components/ui/floating-navbar";
 
 export default function Home() {
   return (
-    <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
-      <div className="max-w-7xlw-full">
-        <h1>Hello portfolio</h1>
-        {/* <FloatingNav navItems={navItems} /> */}
-        <Hero />
-      </div>
-    </main>
+      <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
+        <div className="max-w-7xl w-full">
+          {/* <FloatingNav navItems={navItems} /> */}
+          <Hero />
+          <Grid />
+          {/* <RecentProjects />
+          <Clients />
+          <Experience />
+          <Approach />
+          <Footer /> */}
+        </div>
+      </main>
   );
 }
